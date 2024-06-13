03313b8ce57c57c939e45e00e1b038440c3ad952
billygotclout <williamsoluwademilade@gmail.com>
2024-06-13 09:25:21 +0100

    fixed upload error

--- middleware/isUserActive.js
+++ middleware/isUserActive.js
@@ -4,7 +4,9 @@ const CustomError = require("../utils/CustomError");
 const isUserActive = async (req, res, next) => {
   try {
     const user = await User.findOne({ email: req.body.email });
-
+    if (!user) {
+      throw new CustomError("User not found", 404);
+    }
     if (user.status === "disabled") {
       throw new CustomError(
         "Your account has been disabled. Please contact your administrator for access permissions.",
