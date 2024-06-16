8a5c9448b16534d628b340d11074733f68881de6
billygotclout <williamsoluwademilade@gmail.com>
2024-06-16 09:27:08 +0100

    lets see

--- server.js
+++ server.js
@@ -11,7 +11,13 @@ const port = process.env.PORT || 3001;
 dbConnect();
 app.use(express.json());
 app.use(cors());
-app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+const uploadsDir = path.join(__dirname, "uploads");
+if (!fs.existsSync(uploadsDir)) {
+  fs.mkdirSync(uploadsDir);
+}
+
+// Serve static files from the 'uploads' directory
+app.use("/uploads", express.static(uploadsDir));
 
 app.get("/", (req, res) => {
   res.send("API is running");
@@ -20,7 +26,7 @@ app.use("/api/auth", require("./routes/user.routes"));
 app.use("/api/file", require("./routes/file.routes"));
 app.use("/api/inventory", require("./routes/inventory.routes"));
 app.use("/api/checker", require("./routes/checker.routes"));
-// app.use(errorHandler);
+app.use(errorHandler);
 app.listen(port, () => {
   console.log(`Server is listening on http://localhost:${port}`);
 });
