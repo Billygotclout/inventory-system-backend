01ce5c3e39514f12988ce34f4bf00907ff2ff7d7
billygotclout <williamsoluwademilade@gmail.com>
2024-07-17 10:04:21 +0100

    fix

--- routes/stockout.routes.js
+++ routes/stockout.routes.js
@@ -11,6 +11,7 @@ const roleChecker = require("../middleware/roleChecker");
 const router = express.Router();
 const multer = require("multer");
 const path = require("path");
+const CustomError = require("../utils/CustomError");
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
     cb(null, path.join(__dirname, "../uploads/payments"));
